19753b0c0a8161ad64e688720b32115ad3778a1f
kavya79488 <144649750+kavya79488@users.noreply.github.com>
2023-09-21 21:41:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,40 +2,40 @@ var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 function replacingImage(){
     if(randomNumber1===1){
-        var change = document.querySelectorAll(".image6")[0].setAttribute("src","./images/dice1.png");
+        var change = document.querySelectorAll(".image6")[0].setAttribute("src","https://t3.ftcdn.net/jpg/01/97/16/30/360_F_197163001_H2gm3LqIrVTm7Fne6kclwRDfMcfm57BC.jpg");
     }
     else if(randomNumber1===2){
-        var change = document.querySelectorAll(".image6")[0].setAttribute("src","./images/dice2.png");
+        var change = document.querySelectorAll(".image6")[0].setAttribute("src","https://clipartix.com/wp-content/uploads/2016/09/2-dice-clipart-free-clipart-images-image.png");
     }
     else if(randomNumber1===3){
-        var change = document.querySelectorAll(".image6")[0].setAttribute("src","./images/dice3.png");
+        var change = document.querySelectorAll(".image6")[0].setAttribute("src","https://t3.ftcdn.net/jpg/02/36/03/64/360_F_236036401_3s3HBOxeE5a51a08fOx4UXK6LH8RaZyT.jpg");
     }
     else if(randomNumber1===4){
-        var change = document.querySelectorAll(".image6")[0].setAttribute("src","./images/dice4.png");
+        var change = document.querySelectorAll(".image6")[0].setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVanX8y3_bmVZTeW4rGqLJ_RkB1hpOVRjFpg&usqp=CAU");
     }
     else if(randomNumber1===5){
-        var change = document.querySelectorAll(".image6")[0].setAttribute("src","./images/dice5.png");
+        var change = document.querySelectorAll(".image6")[0].setAttribute("src","https://www.pngkey.com/png/detail/91-917449_dots-clipart-five-five-side-of-dice.png");
     }
     else if(randomNumber1===6){
-        var change = document.querySelectorAll(".image6")[0].setAttribute("src","./images/dice6.png");
+        var change = document.querySelectorAll(".image6")[0].setAttribute("src","https://www.pngitem.com/pimgs/m/614-6141042_library-of-6-dice-number-clip-royalty-free.png");
     }
     if(randomNumber2===1){
-        var change = document.querySelectorAll(".image6")[1].setAttribute("src","./images/dice1.png");
+        var change = document.querySelectorAll(".image6")[1].setAttribute("src","https://t3.ftcdn.net/jpg/01/97/16/30/360_F_197163001_H2gm3LqIrVTm7Fne6kclwRDfMcfm57BC.jpg");
     }
     else if(randomNumber2===2){
-        var change = document.querySelectorAll(".image6")[1].setAttribute("src","./images/dice2.png");
+        var change = document.querySelectorAll(".image6")[1].setAttribute("src","https://clipartix.com/wp-content/uploads/2016/09/2-dice-clipart-free-clipart-images-image.png");
     }
     else if(randomNumber2===3){
-        var change = document.querySelectorAll(".image6")[1].setAttribute("src","./images/dice3.png");
+        var change = document.querySelectorAll(".image6")[1].setAttribute("src","https://t3.ftcdn.net/jpg/02/36/03/64/360_F_236036401_3s3HBOxeE5a51a08fOx4UXK6LH8RaZyT.jpg");
     }
     else if(randomNumber2===4){
-        var change = document.querySelectorAll(".image6")[1].setAttribute("src","./images/dice4.png");
+        var change = document.querySelectorAll(".image6")[1].setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVanX8y3_bmVZTeW4rGqLJ_RkB1hpOVRjFpg&usqp=CAU");
     }
     else if(randomNumber2===5){
-        var change = document.querySelectorAll(".image6")[1].setAttribute("src","./images/dice5.png");
+        var change = document.querySelectorAll(".image6")[1].setAttribute("src","https://www.pngkey.com/png/detail/91-917449_dots-clipart-five-five-side-of-dice.png");
     }
     else if(randomNumber2===6){
-        var change = document.querySelectorAll(".image6")[1].setAttribute("src","./images/dice6.png");
+        var change = document.querySelectorAll(".image6")[1].setAttribute("src","https://www.pngitem.com/pimgs/m/614-6141042_library-of-6-dice-number-clip-royalty-free.png");
     }
 
 }
